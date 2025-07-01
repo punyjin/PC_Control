2003e98785d7b453c3c9703f87bc6606c3d126a7
xorbit <91112547+punyjin@users.noreply.github.com>
2025-07-01 15:45:56 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace NS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.statusTimer = new System.Windows.Forms.Timer(this.components);
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(107, 90);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(89, 38);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(12, 90);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(89, 38);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // statusTimer
+            // 
+            this.statusTimer.Tick += new System.EventHandler(this.statusTimer_Tick);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lbl_status.Location = new System.Drawing.Point(24, 26);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(163, 25);
+            this.lbl_status.TabIndex = 2;
+            this.lbl_status.Text = "Software Status";
+            this.lbl_status.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Text = "notifyIcon1";
+            this.trayIcon.Visible = true;
+            // 
+            // trayMenu
+            // 
+            this.trayMenu.Name = "trayMenu";
+            this.trayMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 143);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Control Panel";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Timer statusTimer;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip trayMenu;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trayMenu = new ContextMenuStrip();
+            trayMenu.Items.Add("เปิดหน้าต่าง", null, ShowWindow);
+            trayMenu.Items.Add("ออกจากโปรแกรม", null, ExitApp);
+
+            trayIcon = new NotifyIcon
+            {
+                Icon = SystemIcons.Application,
+                ContextMenuStrip = trayMenu,
+                Text = "NetSupport Client Controller",
+                Visible = true
+            };
+
+            trayIcon.DoubleClick += ShowWindow;
+
+            // ย่อโปรแกรมไปที่ Tray เมื่อเริ่ม
+            this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false; 
+            trayIcon.BalloonTipTitle = "NetSupport Client";
+            trayIcon.BalloonTipText = "โปรแกรมกำลังทำงานอยู่เบื้องหลัง";
+            trayIcon.ShowBalloonTip(1000);
+            statusTimer.Start();
+            UpdateStatus();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            string exePath = @"C:\Program Files (x86)\NetSupport\NetSupport School\client32.exe";
+
+            try
+            {
+                if (IsProcessRunning("client32"))
+                {
+                    MessageBox.Show("โปรแกรมกำลังทำงานอยู่แล้ว", "แจ้งเตือน", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                if (File.Exists(exePath))
+                {
+                    ProcessStartInfo psi = new ProcessStartInfo
+                    {
+                        FileName = exePath,
+                        UseShellExecute = true,
+                        Verb = "runas" // รันแบบ Admin
+                    };
+
+                    Process.Start(psi);
+                    this.Hide();
+                    this.ShowInTaskbar = false;
+
+                    trayIcon.BalloonTipTitle = "NetSupport Client";
+                    trayIcon.BalloonTipText = "โปรแกรมกำลังทำงานอยู่เบื้องหลัง";
+                    trayIcon.ShowBalloonTip(1000);
+
+                }
+                else
+                {
+                    MessageBox.Show("ไม่พบไฟล์โปรแกรมที่ต้องการรัน", "ข้อผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"เกิดข้อผิดพลาดในการรันโปรแกรม:\n{ex.Message}", "ข้อผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var processes = Process.GetProcessesByName("client32");
+                if (processes.Length == 0)
+                {
+                    MessageBox.Show("ไม่พบโปรแกรมที่กำลังทำงาน", "แจ้งเตือน", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                foreach (var proc in processes)
+                {
+                    proc.Kill();
+                }
+
+                MessageBox.Show("โปรแกรมถูกปิดเรียบร้อยแล้ว", "สำเร็จ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"เกิดข้อผิดพลาดในการปิดโปรแกรม:\n{ex.Message}", "ข้อผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        bool IsProcessRunning(string processName)
+        {
+            return Process.GetProcessesByName(processName).Length > 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ShowWindow(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void ExitApp(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void UpdateStatus()
+        {
+            bool isRunning = IsProcessRunning("client32");
+            lbl_status.Text = isRunning ? "สถานะ: ทำงานอยู่" : "สถานะ: ปิดอยู่";
+            lbl_status.ForeColor = isRunning ? Color.Green : Color.Red;
+
+            // Enable/Disable ปุ่ม
+            btn_start.Enabled = !isRunning;
+            btn_stop.Enabled = isRunning;
+        }
+
+        private void statusTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateStatus();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+                this.ShowInTaskbar = false;
+                trayIcon.BalloonTipTitle = "ยังทำงานอยู่";
+                trayIcon.BalloonTipText = "โปรแกรมยังคงทำงานอยู่ใน System Tray";
+                trayIcon.ShowBalloonTip(2000);
+            }
+            else
+            {
+                base.OnFormClosing(e);
+            }
+        }
+    }
+}
