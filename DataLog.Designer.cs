3356f0430f92f37e08652ecd388f8ff3e2a4cd93
xorbit <91112547+punyjin@users.noreply.github.com>
2025-07-09 12:41:13 +0700

    Add files via upload

---
+++ DataLog.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace NS
+{
+    partial class DataLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datalogTextBox = new System.Windows.Forms.RichTextBox();
+            this.btn_install = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // datalogTextBox
+            // 
+            this.datalogTextBox.Location = new System.Drawing.Point(12, 222);
+            this.datalogTextBox.Name = "datalogTextBox";
+            this.datalogTextBox.Size = new System.Drawing.Size(560, 149);
+            this.datalogTextBox.TabIndex = 0;
+            this.datalogTextBox.Text = "";
+            // 
+            // btn_install
+            // 
+            this.btn_install.Enabled = false;
+            this.btn_install.Location = new System.Drawing.Point(497, 377);
+            this.btn_install.Name = "btn_install";
+            this.btn_install.Size = new System.Drawing.Size(75, 23);
+            this.btn_install.TabIndex = 1;
+            this.btn_install.Text = "Install";
+            this.btn_install.UseVisualStyleBackColor = true;
+            this.btn_install.Click += new System.EventHandler(this.btn_install_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 145);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1.netsh int ip reset\r\n2.netsh winsock reset\r\n3.ipconfig /release\r\n4.ipconfig /ren" +
+    "ew\r\n5.ipconfig /flushdns";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(458, 50);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "เปิด CMD ด้วย Run Admin เมื่อหน้าต่าง CMD ขึ้นมา \r\nให้พิมพ์คำสั่งตามนี้";
+            // 
+            // DataLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 407);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_install);
+            this.Controls.Add(this.datalogTextBox);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 446);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 446);
+            this.Name = "DataLog";
+            this.Text = "DataLog";
+            this.Load += new System.EventHandler(this.DataLog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox datalogTextBox;
+        private System.Windows.Forms.Button btn_install;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ DataLog.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NS
+{
+    public partial class DataLog : Form
+    {
+        public DataLog()
+        {
+            InitializeComponent();
+        }
+
+        private void DataLog_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private bool IsRunAsAdministrator()
+        {
+            return new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+        }
+        private void InstallBoBlox()
+        {
+            if (!IsRunAsAdministrator())
+            {
+                MessageBox.Show("โปรแกรมต้องรันด้วยสิทธิ์ Administrator!", "INSTALLER", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe")
+            {
+                Verb = "runas",
+                UseShellExecute = true // ต้องเป็น true เพื่อให้ CMD ปรากฏขึ้น
+            };
+
+            try
+            {
+                Process.Start(processStartInfo);
+                MessageBox.Show("กรุณารันคำสั่งต่อไปนี้ใน CMD:\nnetsh int ip reset\nnetsh winsock reset\nipconfig /release\nipconfig /renew\nipconfig /flushdns", "INSTALLER", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"เกิดข้อผิดพลาด: {ex.Message}", "INSTALLER", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void RunCommandAsAdministrator(string command)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe")
+            {
+                Arguments = "/C " + command,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                Verb = "runas"
+            };
+
+            try
+            {
+                using (Process process = Process.Start(processStartInfo))
+                {
+                    process.WaitForExit();
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+
+                    // บันทึกคำสั่งที่รันด้วย
+                    datalogTextBox.AppendText($"Command: {command}\n");
+                    if (!string.IsNullOrEmpty(output))
+                    {
+                        datalogTextBox.AppendText("Output: " + output + "\n");
+                    }
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        datalogTextBox.AppendText("Error: " + error + "\n");
+                    }
+                    if (process.ExitCode != 0)
+                    {
+                        datalogTextBox.AppendText($"Exit Code: {process.ExitCode}\n");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                datalogTextBox.AppendText($"Exception in command '{command}': {ex.Message}\n");
+            }
+        }
+
+        private void btn_install_Click(object sender, EventArgs e)
+        {
+            InstallBoBlox();
+        }
+    }
+}
